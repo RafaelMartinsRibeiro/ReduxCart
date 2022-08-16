db5656f73ecde5fb4bd804ff4a5864cb38ce6141
RafaelMartinsRibeiro <rafaelmartins1964@hotmail.com>
2022-08-15 21:15:08 -0300

    Added clearCart action(reducer) in cartSlice

---
+++ src/components/CartContainer.jsx
@@ -0,0 +1,48 @@
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
+import { CartItem } from "./CartItem";
+
+export const CartContainer = () => {
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
+
+  const dispatch = useDispatch();
+  return (
+    <>
+      {amount < 1 ? (
+        <section className="cart">
+          <header>
+            <h2>your bag</h2>
+            <h4 className="empty-cart">is currently empty</h4>
+          </header>
+        </section>
+      ) : (
+        <section className="cart">
+          <header>
+            <h2>your bag</h2>
+            <div>
+              {cartItems.map((item) => (
+                <CartItem key={item.id} {...item} />
+              ))}
+            </div>
+
+            <footer>
+              <hr />
+              <div className="cart-total">
+                <h4>
+                  total <span>${total}</span>
+                </h4>
+              </div>
+
+              <button
+                onClick={() => dispatch(clearCart())}
+                className="btn clear-btn"
+              >
+                clear cart
+              </button>
+            </footer>
+          </header>
+        </section>
+      )}
+    </>
+  );
+};
