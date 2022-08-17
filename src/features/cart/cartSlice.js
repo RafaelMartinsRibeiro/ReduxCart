b8b03c771ec7d1403095e667fe154e8585868549
RafaelMartinsRibeiro <rafaelmartins1964@hotmail.com>
2022-08-16 21:28:34 -0300

    Added calculateTotals reducer

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -29,7 +29,18 @@ const cartSlice = createSlice({
         cartItem.amount = 0;
       }
     },
-    
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+
+      state.amount = amount;
+      state.total = total;
+    },
   },
 });
 
