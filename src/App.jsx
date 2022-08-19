f389c5eb40e68308287a6b09116142ea24c411fd
RafaelMartinsRibeiro <rafaelmartins1964@hotmail.com>
2022-08-18 21:45:38 -0300

    Added cartItems API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { CartContainer } from "./components/CartContainer";
 import { Modal } from "./components/Modal";
 import { Navbar } from "./components/Navbar";
+import { getCartItems } from "./features/cart/cartSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
   const { isOpen } = useSelector((store) => store.modal);
+  const { isLoading } = useSelector((store) => store.cart);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <main>
